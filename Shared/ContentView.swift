f052cce9da354e192e49ff43a72e6db9e97db119
gomathi saminathan <gomathi.gms@gmail.com>
2023-03-29 10:07:26 -0400

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Rajendran Eshwaran on 3/29/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/PIZZACORNERApp.swift
@@ -0,0 +1,17 @@
+//
+//  PIZZACORNERApp.swift
+//  Shared
+//
+//  Created by Rajendran Eshwaran on 3/29/23.
+//
+
+import SwiftUI
+
+@main
+struct PIZZACORNERApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
